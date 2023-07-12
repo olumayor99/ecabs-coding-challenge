d99cdae37a78d20c926490f6a01ebc184ac300f6
olumayor99 <olumayoor99@gmail.com>
2023-07-12 19:11:03 +0300

    added tf

---
+++ EKS-Cluster/eks.tf
@@ -0,0 +1,186 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "eks_vpc" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames        = true
+  enable_dns_support          = true
+}
+
+resource "aws_internet_gateway" "eks_gateway" {
+  vpc_id = aws_vpc.eks_vpc.id
+}
+
+resource "aws_route_table" "eks_route_table" {
+  vpc_id = aws_vpc.eks_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.eks_gateway.id
+  }
+}
+
+# Associate the route table with the subnet
+resource "aws_route_table_association" "eks_association_1" {
+  subnet_id = aws_subnet.eks_subnet_1.id
+  route_table_id = aws_route_table.eks_route_table.id
+}
+
+resource "aws_route_table_association" "eks_association_2" {
+  subnet_id = aws_subnet.eks_subnet_2.id
+  route_table_id = aws_route_table.eks_route_table.id
+}
+
+resource "aws_security_group" "eks_security_group" {
+  name        = "eks_security_group"
+  vpc_id = aws_vpc.eks_vpc.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_subnet" "eks_subnet_1" {
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = "10.0.5.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "eks_subnet_2" {
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = "10.0.7.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_eks_cluster" "eks_cluster" {
+  name     = "blue-green-cluster"
+  role_arn = aws_iam_role.eks_cluster_role.arn
+  version  = "1.27"
+
+  vpc_config {
+    subnet_ids = [
+      aws_subnet.eks_subnet_1.id,
+      aws_subnet.eks_subnet_2.id,
+    ]
+  }
+}
+
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "blue-green-cluster-role"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy_attachment" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_service_policy_attachment" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  role       = aws_iam_role.eks_cluster_role.name
+}
+
+resource "aws_eks_node_group" "bg_node_group" {
+  cluster_name    = aws_eks_cluster.eks_cluster.name
+  node_group_name = "bg-node-group"
+  node_role_arn   = aws_iam_role.bg_node_group_role.arn
+  subnet_ids      = [aws_subnet.eks_subnet_1.id, aws_subnet.eks_subnet_2.id,] 
+  instance_types  = ["t3.medium"]
+  scaling_config {
+    desired_size = 3
+    min_size     = 2
+    max_size     = 5
+  }
+}
+
+resource "aws_iam_role" "bg_node_group_role" {
+  name = "bg-node-group-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "bg_node_group_role_attachment" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.bg_node_group_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "bg_node_group_ecr_policy_attachment" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.bg_node_group_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "bg_node_group_cni_policy_attachment" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.bg_node_group_role.name
+}
+
+
